a79e0979a267bf332b6f7cad9754c83a30357488
Eryko <eryko4350@gmail.com>
2024-09-16 16:12:34 -0300

    meu 1 commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Jornada de Dados")
+
+#variáveis
+idade = "Luciano"
+idade = 35
+
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#listas
+lista= [1,2,3,4]
+
+#metodos
+lista_invertida = lista.reverse()
+print(lista_invertida)
