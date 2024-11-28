02557ba2ff03d9af6c81dd5927d972789bb909ce
Eryko <eryko4350@gmail.com>
2024-11-28 14:28:06 -0300

    inicio do desafio

---
+++ python1.py
@@ -0,0 +1,29 @@
+# print("teste 1")
+# print(3)
+# print(3+4)
+#print("olá"+ " "+ "pessoal")
+#print("Olá," + input("Digite seu nome: ")+ "!")
+
+
+#criando um programa que digita um nome e retorno o número de caracteres
+
+#print(len(input("Digite seu nome: ")))
+
+# print(int(input("Digite um número: ")) + int(input("Digite outro número: ")))
+
+#quantidade de letras usando variável
+
+# nome = input("Qual seu nome: ")
+# quant_letras = len(nome)
+# print(quant_letras)
+
+#Cauculo de Bônus de Usuário
+CONSTANTE_BONUS = 1000
+
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salário: "))
+bonus = float(input("Digite seu bônus: "))
+
+valor_bonus = CONSTANTE_BONUS + salario * bonus
+
+print(f"Olá {nome} você possui um bônus de {valor_bonus}")
